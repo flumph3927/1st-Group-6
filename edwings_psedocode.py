16d660fadf8997aa056c2c46e0f9c441caa6a30c
edwingsantos <edwing.santos@ucas-edu.net>
2026-01-22 09:52:39 -0700

    edwing's psedocode, I relized i need to make the library to the main

---
+++ edwings_psedocode.py
@@ -0,0 +1,35 @@
+#make a funtion create
+
+
+#Make a variable named create
+#make random equals random.randint(5,10) so it pulls a random num from 5-10
+
+#make name input what would you like to name to be
+
+#make a while loop 
+
+#print the names of the name of the classes the user can choose with numbers next to them 
+
+#make class equals input what their character class is going to be, with numbers to be easier
+
+#if class == 1, print you have choosen a swordsman and append to the empty library.
+    #call distribute
+    #print stats
+    #print skill and say that to unlock another one they need to level up
+    #call main
+
+#elif class == 2, print you have choose wizard and append wizard to the emtpy library
+    #call distribute 
+    #print stats
+    #print skill and say that to unlock another one they need to level up
+    #call main
+
+
+#elif class == 3, print you have arhcer wizard and append archer to the empty library
+    #call distribute
+    #print stats
+    #print skill and say that to unlock another one they need to level up
+    #call main
+
+#else print to selct an actual number 
+    #continue
